23cecb68c5f20258a049ca0c3dfbe648ed14b414
JoJunHwa <jjj5306@naver.com>
2024-05-24 14:28:40 +0900

    feat : add main page

--- Ryugiohproject/Ryugiohproject_app/views.py
+++ Ryugiohproject/Ryugiohproject_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
+
+def index(request):
+    cards = Card.objects.all()
+    return render(request, 'index.html', {'cards': cards})
