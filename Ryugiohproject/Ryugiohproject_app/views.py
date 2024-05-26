faabf31d4e1eafff6be2eb075e03a140803a6b9d
JoJunHwa <jjj5306@naver.com>
2024-05-26 13:28:48 +0900

    feat database init, make using external api function, make service layer

--- Ryugiohproject/Ryugiohproject_app/views.py
+++ Ryugiohproject/Ryugiohproject_app/views.py
@@ -1,8 +1,19 @@
+# Ryugiohproject_app/views.py
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Card
+from .services import CardService
 
-# Create your views here.
-
+# Main page view
 def index(request):
     cards = Card.objects.all()
     return render(request, 'index.html', {'cards': cards})
+
+# API data fetching and saving to database view
+def cardinfo(request):
+    result = CardService.fetch_and_save_card_data()
+    if 'message' in result:
+        return JsonResponse(result, status=200)
+    else:
+        return JsonResponse(result, status=500)
